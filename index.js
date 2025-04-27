2719f4cc2db2cb2febca1ed74bab3cbcdf8ad841
korczak1i2021 <106950311+korczak1i2021@users.noreply.github.com>
2025-04-28 00:35:53 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+let rows
+let cols
+let seats = []
+let editState = true
+
+function redirect() {
+    document.getElementById("generator").style.display = "none"
+    document.getElementById("seats").style.display = "block"
+}
+
+function generateSeats() {
+    for (let y = 0; y < rows; y++) {
+        let row = []
+        for (let x = 0; x < cols; x++) {
+            row.push("w")
+        }
+        seats.push(row)
+    }
+}
+
+function updateSeatsButtons() {
+    let containerSeats = document.getElementById("container_seats")
+    containerSeats.innerHTML = ""
+    console.log(seats)
+    for (let y = 0; y < rows; y++) {
+        containerSeats.innerHTML += '<div class="container_seats_row">'
+
+        for (let x = 0; x < cols; x++) {
+            if (seats[y][x] === 'w') {
+                containerSeats.innerHTML += '<button class="container_seats_seat seat_empty" onclick = "reservarte(' + y + ',' + x + ') ">' + (y + 1) + '/' + (x + 1) + '</button>'
+            }
+            else if (seats[y][x] === 'e') {
+                containerSeats.innerHTML += '<button class="container_seats_seat seat_chosen" onclick = "reservarte(' + y + ',' + x + ') ">' + (y + 1) + '/' + (x + 1) + '</button>'
+            }
+            else {
+                containerSeats.innerHTML += '<button class="container_seats_seat seat_taken">' + (y + 1) + '/' + (x + 1) + '</button>'
+            }
+        }
+        containerSeats.innerHTML += '</div>'
+    }
+}
+
+function generation() {
+    rows = document.getElementById("rows").value
+    cols = document.getElementById("cols").value
+
+    redirect()
+    generateSeats()
+    updateSeatsButtons()
+}
+
+function reservarte(y, x) {
+    if (editState === true) {
+        if (seats[y][x] === 'w') {
+            seats[y][x] = 'e'
+        }
+        else {
+            seats[y][x] = 'w'
+        }
+        updateSeatsButtons()
+    }
+}
+
+function chosenSeats() {
+    editState = false
+    let reseved = []
+    for (let y = 0; y < rows; y++) {
+        for (let x = 0; x < cols; x++) {
+            if (seats[y][x] === 'e') {
+                reseved.push([y + 1, x + 1])
+            }
+        }
+    }
+    let seatList = document.getElementById('container_seats_list')
+    seatList.innerHTML = "<ul>"
+    for (let i = 0; i < reseved.length; i++) {
+        seatList.innerHTML += '<li> rząd: ' + reseved[i][0] + ' miejsce: ' + reseved[i][1] + '&nbsp;<label for="list_checkbox_' + i + '"> bilet ulgowy: </label> <input type="checkbox" id="list_checkbox_' + i + '" class="list_checkbox"></li>'
+    }
+    "</ul>"
+    document.getElementsByClassName("container_seats_reservation")[0].style.display = "block"
+    document.getElementById("container_order").style.display = "block"
+}
+
+
+function changeChoise() {
+    editState = true
+    document.getElementsByClassName("container_seats_reservation")[0].style.display = "none"
+    let seatList = document.getElementById('container_seats_list')
+    seatList.innerHTML = ""
+    document.getElementById("container_order").style.display = "none"
+
+}
+
+function placeOrder() {
+    document.getElementById("container_new-order").style.display = "block"
+    let container_price = document.getElementById("container_price")
+    container_price.style.display = "block"
+    let list_checkbox = document.getElementsByClassName("list_checkbox")
+    let suma = 0
+    for (let i = 0; i < list_checkbox.length; i++) {
+        if (list_checkbox[i].checked === true){
+            suma += 25 
+        }
+        else{
+            suma += 30
+        }
+    }
+    container_price.innerText = suma
+}
+
+
+
